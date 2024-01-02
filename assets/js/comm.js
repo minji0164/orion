dd236077368e8d41279ed6b16e01ac41a2edba3a
minji0164 <min016425@gmail.com>
2024-01-02 18:20:25 +0900

    dd

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,5 +1,10 @@
 var swiper = new Swiper(".slide-intro2", {
+  loop: true,
   pagination: {
     el: ".swiper-pagination",
   },
+//   autoplay: {
+//     delay: 5000, // 슬라이드 간의 간격 (밀리초 단위)
+//     disableOnInteraction: false, // 유저 상호 작용 후에도 자동 재생을 계속 활성화할지 여부
+// },
 });
