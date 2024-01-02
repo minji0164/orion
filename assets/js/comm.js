641e4029ac7f422380b00428f08db691a267ae57
minji0164 <min016425@gmail.com>
2024-01-02 17:41:22 +0900

    dd

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -1,5 +1,5 @@
-new Swiper(".swiper-container", {
-    direction: "vertical", // 수직 슬라이드
-    autoplay: true, // 자동 재생 여부
-    loop: true, // 반복 재생 여부
-  });
+var swiper = new Swiper(".slide-intro2", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
