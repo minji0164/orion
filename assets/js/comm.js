26357bbb7fe4d0f55ad7ce63324c8f4dbe918b43
minji0164 <min016425@gmail.com>
2024-01-08 18:19:03 +0900

    ddd

--- assets/js/comm.js
+++ assets/js/comm.js
@@ -4,7 +4,7 @@ var swiper = new Swiper(".slide-intro2", {
     el: ".swiper-pagination",
   },
 //   autoplay: {
-//     delay: 5000, // 슬라이드 간의 간격 (밀리초 단위)
+//     delay: 3000, // 슬라이드 간의 간격 (밀리초 단위)
 //     disableOnInteraction: false, // 유저 상호 작용 후에도 자동 재생을 계속 활성화할지 여부
 // },
 });
@@ -12,3 +12,9 @@ var swiper = new Swiper(".slide-intro2", {
 var swiper = new Swiper(".slide-biz", {
   spaceBetween: 30,
 });
+
+AOS.init({
+  duration: 1200,
+})
+
+
